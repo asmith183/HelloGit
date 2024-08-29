c7c18883879211c3aa38c51791637c1e087c77ef
notth <notth@LiamPuter.unl.edu>
2024-08-29 11:56:02 -0500

    Liam

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("My name is Aden Smith!");
 		System.out.println("My major is Comp Sci.");
+		System.out.println("My name is Liam Makela!");
+		System.out.println("My major is Comp Sci.");
 	}
 }
